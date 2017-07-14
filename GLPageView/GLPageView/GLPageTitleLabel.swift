a2d785792fca0a5e0e6e5db41b3aaf24d8a29a39
GLei1122 <qfb911122@gmail.com>
2017-07-14 09:54:20 +0800

    no message

---
+++ GLPageView/GLPageView/GLPageTitleLabel.swift
@@ -0,0 +1,21 @@
+//
+//  GLPageTitleLabel.swift
+//  GLPageView
+//
+//  Created by GL on 2017/7/14.
+//  Copyright © 2017年 GL. All rights reserved.
+//
+
+import UIKit
+
+class GLPageTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
